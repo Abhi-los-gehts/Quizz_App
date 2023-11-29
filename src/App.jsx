ed0ded32ba740784877fd365eed6c3afc0b8baf1
Abhi <abhilasha.reva61@gmail.com>
2023-11-29 10:43:25 +0100

    Created Navbar,datajson file, Added plugins

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css';
+import Navbar from './components/Layout/Navbar';
 
 function App() {
   return (
     <div className="App">
       
+{/* navbar */}
+<Navbar />
+  
+
     </div>
   );
 }
